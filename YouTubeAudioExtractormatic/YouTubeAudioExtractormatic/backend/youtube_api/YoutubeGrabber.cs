f331415d5ab847f27f8a63a77a7c74e6188835ef
David Mortiboy <accounts@davidmortiboy.com>
2016-09-11 12:13:15 +0100

    added reference to the YouTube api and created YouTubeGrabber class

---
+++ YouTubeAudioExtractormatic/YouTubeAudioExtractormatic/backend/youtube_api/YoutubeGrabber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.YouTube.v3;
+using Google.Apis.Services;
+
+namespace YouTubeAudioExtractormatic
+{
+    /// <summary>
+    /// Provides functionality to interface with the Google YouTube v3 api
+    /// </summary>
+    public class YoutubeGrabber
+    {
+        private static string apikey = "AIzaSyAApYbIaWadnBvNbpWHn_QrfqZRx7mGMqM";
+        YouTubeService youtubeService;
+
+        /// <summary>
+        /// Initialise the YouTube service with apikey
+        /// </summary>
+        public YoutubeGrabber()
+        {
+            var ys = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = apikey,
+                ApplicationName = this.GetType().ToString()
+            });
+
+            this.youtubeService = ys;
+        }
+    }
+}
